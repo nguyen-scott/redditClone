1764f3c6a5d288583130d1732e97e21052d87ab0
Scott Nguyen <scoot3r90@gmail.com>
2019-09-20 09:09:38 -0700

    styling and added service for posts

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
 app.controller('MainCtrl', [
   '$scope',
-  function($scope){
-    $scope.posts = [
-      {title: 'post1', upvotes: 5},
-      {title: 'post2', upvotes: 2},
-      {title: 'post3', upvotes: 15},
-      {title: 'post4', upvotes: 9},
-      {title: 'post5', upvotes: 4}
-    ];
+  'posts',
+  function($scope, posts){
+    $scope.posts = posts.posts;
+    // $scope.posts = [
+    //   {title: 'post1', upvotes: 5},
+    //   {title: 'post2', upvotes: 2},
+    //   {title: 'post3', upvotes: 15},
+    //   {title: 'post4', upvotes: 9},
+    //   {title: 'post5', upvotes: 4}
+    // ];
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === ''){return;}
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
       $scope.title = '';
+      $scope.link = '';
     };
+
+    $scope.incrementUpVotes = function(post){
+      post.upvotes += 1;
+    }
 
   }]
 );
