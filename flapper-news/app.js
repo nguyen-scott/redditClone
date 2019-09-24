c30c0ca5ebb98ec90213b5379f73f541fecca5bf
Scott Nguyen <scoot3r90@gmail.com>
2019-09-24 16:34:50 -0700

    added API routes

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,3 +1,9 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
